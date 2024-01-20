5eb16402390ae24292970c58deb5aa31f7d3fa17
VitaliyYurkevich <vitaliy.yurkevich@yandex.ru>
2024-01-20 23:00:00 +0300

    clear test

--- src/features/TodolistList/app-reducer.test.ts
+++ src/features/TodolistList/app-reducer.test.ts
@@ -1,4 +1,4 @@
-import {appReducer, InitialStateType, setAppErrorAC, setAppStatusAC} from "../app/app-reducer";
+import {appReducer, InitialStateType, setAppErrorAC, setAppStatusAC} from "../../app/app-reducer";
 
 
 let startState: InitialStateType
@@ -14,14 +14,14 @@ beforeEach(()=>{
 
 test('correct error message should be set', () => {
 
-    const endState = appReducer(startState, setAppErrorAC('some error'))
+    const endState = appReducer(startState, setAppErrorAC({error:'some error'} ))
     expect(endState.error).toBe('some error')
 
 })
 
 test('correct status should be set', () => {
 
-    const endState = appReducer(startState, setAppStatusAC('loading'))
+    const endState = appReducer(startState, setAppStatusAC({status:'loading'}))
     expect(endState.status).toBe('loading')
 
 })
